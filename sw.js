9ca1d0a39bdd20cb5e42af80fffc6d701dc79238
along222534 <cheeta5443@gmail.com>
2025-07-11 17:15:27 +0700

    แก้ไข

--- sw.js
+++ sw.js
@@ -1,14 +1,7 @@
-// service-worker.js
 self.addEventListener("install", event => {
-  self.skipWaiting(); // บังคับให้ใช้เวอร์ชันใหม่ทันที
-  console.log("[SW] Installed");
+  console.log("[ServiceWorker] Installed");
 });
 
-self.addEventListener("activate", event => {
-  clients.claim(); // ให้ควบคุมทุก tab ทันที
-  console.log("[SW] Activated");
-});
-
-self.addEventListener("fetch", event => {
-  // ไม่ cache อะไรไว้เลย เพื่อลดปัญหาเวอร์ชัน
+self.addEventListener("fetch", () => {
+  // future caching here
 });
